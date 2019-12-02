45fb6c7e6f95dc96044b0d60609d8bca8b9bf8b8
kevin <kevin.obuya@systechafrica.com>
2019-12-02 22:46:24 +0300

    minor refactoring

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from Oyasis import oyasis
 config = oyasis.Ini()
 # acquire new session using the config deta
 session = oyasis.Session(ini=config)
-#start translation
+# start translation
 tafsiri_work = oyasis.Tafsiri(session=session)
-#select random string
-while(True):
-    random_string_dict=tafsiri_work.get_random_string()
-    print(random_string_dict["component"]+" -> "+random_string_dict["RandString"])
+# select random string
+while True:
+    random_string_dict = tafsiri_work.get_random_string()
+    print(random_string_dict["component"] + " -> " + random_string_dict["RandString"])
     translated = input("Translation: ")
-    tafsiri_work.translate(translation=translated,todo=random_string_dict)
+    tafsiri_work.translate(translation=translated, todo=random_string_dict)
