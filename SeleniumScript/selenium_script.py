984f7912929b38ac14894638bf007efbbaaa41d7
kevobuya <kevobuya@gmail.com>
2019-12-03 03:02:43 +0300

    refactoring

--- SeleniumScript/selenium_script.py
+++ SeleniumScript/selenium_script.py
@@ -4,7 +4,7 @@ import configparser
 
 
 class SeleniumScript:
-    
+
     def __init__(self):
         self.config = configparser.ConfigParser().read('config.ini')
         self.chromedriver_location = self.config['SELENIUM']['chrome_webdriver_location']
@@ -14,7 +14,7 @@ class SeleniumScript:
         self.chrome_options.add_argument("disable-gpu")
         self.chrome_options.add_argument("window-size=1440,900")
         self.driver = webdriver.Chrome(self.chromedriver_location, options=self.chrome_options)
-        
+
     def navigate_page(self, url, cookie):
         self.driver.get(url)
         self.driver.add_cookie(self.construct_cookie(cookie, url))
@@ -24,13 +24,14 @@ class SeleniumScript:
     def construct_cookie(cookie, url):
         sessionid_value = cookie[1]
         domain = url.split("//")[1].split("/")[0]
-        constructed_cookie_dict = {"name":"sessionid","domain":domain,"value":sessionid_value}
+        constructed_cookie_dict = {"name": "sessionid", "domain": domain,
+                                   "value": self.config['WEBLATE']['fallback_cookie']}
         return constructed_cookie_dict
-        
+
     def set_translation(self, xpath, translation):
         self.driver.find_element_by_xpath(xpath).clear()
         self.driver.find_element_by_xpath(xpath).send_keys(translation)
-        
+
     def send_translation(self, xpath):
         self.driver.find_element_by_xpath(xpath).click()
 
