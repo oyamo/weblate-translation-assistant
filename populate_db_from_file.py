495c3b862357a986519b53fdc557a8383fc4d2f1
kevobuya <kevobuya@gmail.com>
2019-12-03 04:56:17 +0300

    added sql queries to create tables if not exist

--- populate_db_from_file.py
+++ populate_db_from_file.py
@@ -7,6 +7,35 @@ config.read('config.ini')
 english_string_obj_file = open(config['PERSISTENCE']['untranslated_strings_file_path'], "r")
 english_string_obj_file_contents = english_string_obj_file.readlines()
 conn = sqlite3.connect(config['PERSISTENCE']['database'])
+localisation_main_table_sql = '''
+create table if not exists localisation_main
+(
+    phrase_id           integer
+        constraint localisation_main_pk
+            primary key,
+    package             text not null,
+    english_phrase      text not null,
+    swahili_translation text,
+    translator          text,
+    verified            integer,
+    checksum            text,
+    phrase_url          text,
+    offset              int,
+    passed_to_weblate   text
+);
+
+'''
+
+translator_ids_table_sql = '''create table if not exists translators_ids
+(
+    user_id    int not null
+        constraint translators_ids_pk
+            primary key,
+    first_name text,
+    points     int
+);'''
+conn.execute(localisation_main_table_sql)
+conn.execute(translator_ids_table_sql)
 cursor = conn.cursor()
 links = []
 
